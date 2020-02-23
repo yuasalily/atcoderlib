755726a41299345be1f0ea95b684a87ef0abf69c
yuasalily <yuasalily1011@gmail.com>
2020-02-24 03:13:37 +0900

    fixed

--- cpplib/number.cpp
+++ cpplib/number.cpp
@@ -15,11 +15,3 @@ long long int modpow(long long int a,long long int n,long long int mod){
     }
     return x;
 }
-
-long long int modcomb(long long n, long long k, long long int mod){
-    long long ans = 1;
-    for (long long int i = 0;i < k;i++){
-        ans = (ans * (n-i) % mod ) * modpow(i+1,mod-2,mod) % mod;
-    }
-    return ans;
-}
--- pylib/graph.py
+++
@@ -1,7 +0,0 @@
-"""pythonでグラフ理論"""
-
-class Dijkstra:
-    """ダイクストラ法"""
-    def __init__(self, adjacency_matrix):
-        self.adjacency_matrix = adjacency_matrix
-        
