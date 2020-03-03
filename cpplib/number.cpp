affb0cf3c733e01a7947c8ac5e56bcd57cfff637
yuasalily <yuasalily1011@gmail.com>
2020-03-04 01:40:27 +0900

    added ModInt, modcomb for cpp

--- cpplib/number.cpp
+++ cpplib/number.cpp
@@ -1,17 +1,61 @@
-//mod付きa^n
-long long int modpow(long long int a,long long int n,long long int mod);
-//mod付きnCk
-long long int modcomb(long long n, long long k, long long int mod);
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<numeric>
+#include<string>
+#include<cmath>
+using namespace std;
 
-long long int modpow(long long int a,long long int n,long long int mod){
-    long long int x = 1,a = 2;
-
-    while (n){
-        if (n & 1){
-            x = (x % mod) * (a % mod);
-        }
-        n >>= 1;
-        a = (a % mod) * (a % mod);
+class ModInt{
+    public:
+    long long int x;
+    long long int mod;
+	ModInt(){cout << "test";}
+    ModInt(long long int _x, long long int _mod = 1000000007){
+        this->x = _x % _mod;
+        this->mod = _mod;
     }
-    return x;
+	ModInt operator+(ModInt other){
+		return ModInt(this->x + (other.x % this->mod),this->mod);
+	}
+	ModInt operator-(ModInt other){
+		long long int res = this->x - (other.x % this->mod);
+		if (res < 0) return ModInt(res + this->mod, this->mod);
+		return ModInt(res);
+	}
+	ModInt operator*(ModInt other){
+		return ModInt(this->x * (other.x % this->mod),this->mod);
+	}
+	ModInt operator/(ModInt other){
+		return ModInt(*this * other.inv());
+	}
+	ModInt pow(long long int n){
+		ModInt res(1, this->mod), a(this->x, this->mod);
+		while (n){
+			if (n & 1){
+				res = res * a;
+			}
+			n >>= 1;
+			a = a * a;
+		}
+		return res;
+	}
+	ModInt inv(){
+		return ModInt(this->pow(this->mod - 2));
+	}
+	void disp(){
+		cout << this->x << endl;
+	}
+};
+ostream& operator<<(ostream& stream, const ModInt m){
+	stream << to_string(m.x);
+	return stream;
+}
+ModInt modcomb(long long int n, long long int r, long long int _mod = 1000000007){
+	ModInt x(1, _mod), y(1, _mod);
+	for (long long int i = 0; i < r; i++){
+		x = x * ModInt(n - i);
+		y = y * ModInt(i + 1);
+	}
+	return x / y;
 }
