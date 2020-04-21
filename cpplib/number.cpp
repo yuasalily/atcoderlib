e7245eae273dfc81a1a8dcc8ac85b38cd22310ad
yuasalily <yuasalily1011@gmail.com>
2020-04-22 02:53:17 +0900

    Updated number.cpp

--- cpplib/number.cpp
+++ cpplib/number.cpp
@@ -4,34 +4,59 @@
 #include<numeric>
 #include<string>
 #include<cmath>
+#include<map>
+#include<set>
 using namespace std;
 //c++で整数処理
 
 //mod計算用のクラス
+// ModInt a = {1,1000000007}みたいな定義もできる.
+//modがデフォルト(1000000007)じゃない場合は(ModInt)a+(int)xみたいなものに注意.デフォルトを書き換えれば大丈夫.
+//「各メンバーに対して、コピーコンストラクターあるいは代入演算子を順に呼び出す」 という仕様になっている.
+// (int)1 + (ModInt)10 みたいなことは出来ない.非メンバで定義する必要がある（と思う）.
 class ModInt{
 public:
     long long int x;
     long long int mod;
 	ModInt(){}
     ModInt(long long int _x, long long int _mod = 1000000007){
-        this->x = _x % _mod;
+        this->x = ((_x % _mod) + _mod) % _mod;
         this->mod = _mod;
     }
-	ModInt operator+(ModInt other){
-		return ModInt(this->x + (other.x % this->mod),this->mod);
+	ModInt operator-() const {
+		return ModInt(-this->x, this->mod);
 	}
-	ModInt operator-(ModInt other){
+	ModInt operator+(const ModInt &other) const {
+		return ModInt(this->x + (other.x % this->mod), this->mod);
+	}
+	ModInt operator+=(const ModInt &other){
+		*this = *this + other;
+		return *this;
+	}
+	ModInt operator-(const ModInt &other) const {
 		long long int res = this->x - (other.x % this->mod);
 		if (res < 0) return ModInt(res + this->mod, this->mod);
-		return ModInt(res);
+		return ModInt(res, this->mod);
 	}
-	ModInt operator*(ModInt other){
+	ModInt operator-=(const ModInt &other) {
+		*this = *this - other;
+		return *this;
+	}
+	ModInt operator*(const ModInt &other) const {
 		return ModInt(this->x * (other.x % this->mod),this->mod);
 	}
-	ModInt operator/(ModInt other){
+	ModInt operator*=(const ModInt &other) {
+		*this = *this * other;
+		return *this;
+	}
+	ModInt operator/(const ModInt &other) const {
 		return ModInt(*this * other.inv());
 	}
-	ModInt pow(long long int n){
+	ModInt operator/=(const ModInt &other) {
+		*this = *this / other;
+		return *this;
+	}
+	ModInt pow(long long int n) const {
 		ModInt res(1, this->mod), a(this->x, this->mod);
 		while (n){
 			if (n & 1){
@@ -42,7 +67,7 @@ public:
 		}
 		return res;
 	}
-	ModInt inv(){
+	ModInt inv() const {
 		return ModInt(this->pow(this->mod - 2));
 	}
 	void disp(){
@@ -64,28 +89,57 @@ ModInt modcomb(long long int n, long long int r, long long int _mod = 1000000007
 	return x / y;
 }
 
+//mod nCrを返す関数.何回も計算するときはこっち.ModIntが必要.
+class modcomb{
+	public:
+		vector<ModInt> fact;
+		vector<ModInt> ifact;
+		modcomb(){}
+		modcomb(int n){
+			this->fact.resize(n + 1, ModInt(1));
+			this->ifact.resize(n + 1, ModInt(1));
+			for (int i = 1; i <= n;i++)
+				this->fact[i] = this->fact[i - 1] * i;
+			this->ifact[n] = fact[n].inv();
+			for (int i = n; i >= 1; --i)
+				this->ifact[i - 1] = ifact[i] * i;
+		}
+		ModInt operator()(int n, int k){
+			if (k < 0 || k > n)
+				return 0;
+			return this->fact[n] * this->ifact[k] * this->ifact[n - k];
+		}
+};
+
 //重複組み合わせ(nHr)を返す.modcomb,ModIntが必要
 ModInt H(long long int n, long long int r, long long int _mod = 1000000007){
 	return modcomb(n + r - 1, r, _mod);
 }
 
-//mod nCrを返す関数.何回も計算するときはこっち.未検証
-long long int modcomb(int n, int r, int mod = 1000000007){
-	if (n < r) return 0;
-    if (n < 0 || r < 0) return 0;
-	static int flag = 1;
-	static vector<long long int> fac(n+1,1);
-	static vector<long long int> finv(n+1,1);
-	static vector<long long int> inv(n+1,1);
-	if (flag){
-		for (int i = 2; i < n+1; i++){
-			fac[i] = fac[i - 1] * i % mod;
-			inv[i] = mod - inv[mod%i] * (mod / i) % mod;
-			finv[i] = finv[i - 1] * inv[i] % mod;
-  		}
-		flag = 0;
+//nCrを返す関数.
+long long int comb(long long int n, long long int r){
+	long long int x = 1, y = 1;
+	if (n-r < r) r = n - r;
+	for (long long int i = 0; i < r; i++)
+	{
+		x = x * (n - i);
+		y = y * (i + 1);
 	}
-    return fac[n] * (finv[r] * finv[n - r] % mod) % mod;
+	return x / y;
+}
+
+//繰り返し2乗法を用いたべき乗.x^nを返す.
+long long int pow(long long int x,long long int n){
+	long long int res = 1;
+	while (n)
+	{
+		if (n&1){
+			res *= x;
+		}
+		n >>= 1;
+		x = x * x;
+	}
+	return res;
 }
 
 //累積和を返す関数.
@@ -111,7 +165,7 @@ vector<vector<int>> csum2d(vector<vector<int>> a){
 	return res;
 }
 
-//最大公約数を返す関数.未検証
+//最大公約数を返す関数.
 long long int gcd(long long int x, long long int y){
     if (x < y) swap(x,y);
     while (y > 0){
@@ -121,9 +175,10 @@ long long int gcd(long long int x, long long int y){
     }
     return x;
 }
-//最小公倍数を返す関数.未検証
+
+//最小公倍数を返す関数.gcdが必要.
 long long int lcm(long long int x, long long int y){
-    return x * y / gcd(x,y);
+	return x / gcd(x, y) * y;
 }
 
 //素数かどうかを判定する関数.
@@ -133,4 +188,63 @@ int isprime(long long int n){
         if (n % i == 0) return 0;
     }
     return 1;
+}
+
+//nの約数を返す関数.sortされてない.
+vector<long long int> divisors(long long int n){
+	vector<long long int> res;
+	for (long long int i = 1; i * i <= n;i++){
+		if (n % i == 0){
+			res.push_back(i);
+			if(i * i != n){
+				res.push_back(n / i);
+			}
+		}
+	}
+	return res;
+}
+
+//aとbの公約数を列挙する.divisors,gcdが必要.sortされていない.
+vector<long long int> common_divisors(long long int a, long long int b){
+	long long int g = gcd(a, b);
+	return divisors(g);
+}
+
+//素因数分解をする関数.
+map<long long int, int> prime_factorization(long long int a)
+{
+	map<long long int, int>ans;
+	for (long long i = 2; i*i <= a; ++i) {
+		while (a%i == 0) {
+			ans[i]++;
+			a /= i;
+		}
+	}
+	if (a != 1)ans[a]++;
+	return ans;
+}
+
+//与えられた配列の最小公倍数を返す.返り値は素因数分解された形.long*longがオーバーフローするときはこっちを使う.aによっては遅い.
+//prime_factorizationが必要.
+map<long long int,int> lcm(vector<long long int> &a){
+	int n = a.size();
+	map<long long int, int> res;
+	for (long long int i = 0; i < n; i++)
+	{
+		map<long long int, int> t = prime_factorization(a[i]);
+		for (auto x:t){
+			res[x.first] = max(res[x.first], x.second);
+		}
+	}
+	return res;
+}
+
+//オイラーのファイ関数.1からnまででnと互いに素のものを返す.
+int totient(long long int n){
+	map<long long int, int> a = prime_factorization(n);
+	for (auto x:a)
+	{
+		n = n / x.first * (x.first - 1);
+	}
+	return n;
 }
